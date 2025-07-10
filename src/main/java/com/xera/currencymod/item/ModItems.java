1acd8325ff0fbab52241a6bf4a43f2f6a5a32fe9
xerassh <codexeraad@gmail.com>
2025-07-11 00:25:58 +0200

    Updated the mod to 1.21.3

--- src/main/java/com/xera/currencymod/item/ModItems.java
+++ src/main/java/com/xera/currencymod/item/ModItems.java
@@ -3,22 +3,26 @@ package com.xera.currencymod.item;
 import com.xera.currencymod.CurrencyMod;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 
+import java.util.function.Function;
+
 public class ModItems {
-    public static final Item GOLD_COIN = registerItem("gold_coin", new Item(new Item.Settings()));
-    public static final Item SILVER_COIN = registerItem("silver_coin", new Item(new Item.Settings()));
-    public static final Item BRONZE_COIN = registerItem("bronze_coin", new Item(new Item.Settings()));
-    public static final Item FIGHTERS_COIN = registerItem("fighters_coin", new Item(new Item.Settings()));
+    public static final Item GOLD_COIN = registerItem("gold_coin", Item::new);
+    public static final Item SILVER_COIN = registerItem("silver_coin", Item::new);
+    public static final Item BRONZE_COIN = registerItem("bronze_coin", Item::new);
+    public static final Item FIGHTERS_COIN = registerItem("fighters_coin", Item::new);
 
 
 
-    private static Item registerItem(String name, Item item) {
-        return Registry.register(Registries.ITEM, Identifier.of(CurrencyMod.MOD_ID, name), item);
+    private static Item registerItem(String name, Function<Item.Settings, Item> function) {
+        return Registry.register(Registries.ITEM, Identifier.of(CurrencyMod.MOD_ID, name),
+                function.apply(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(CurrencyMod.MOD_ID, name)))));
     }
 
     public static void registerModItems() {
